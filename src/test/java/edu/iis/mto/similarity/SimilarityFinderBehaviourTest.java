b40c72119c4aa92acb183683afdc9463f7b49512
Kadłubek <179640@edu.p.lodz.pl>
2016-04-13 23:17:27 +0200

    created behaviour tests

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -1,4 +1,46 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
 public class SimilarityFinderBehaviourTest {
+
+    private SimilarityFinder similarityFinder;
+    private MockSequenceSearcher mockSequenceSearcher;
+
+    public static final int[] EMPTY_SEQUENCE = {};
+    public static final int[] SEQUENCE_ONE = {1};
+    public static final int[] SEQUENCE_TWO = {2,3,4};
+
+
+
+    @Before
+    public void setUp(){
+        mockSequenceSearcher = new MockSequenceSearcher();
+        similarityFinder = new SimilarityFinder(mockSequenceSearcher);
+    }
+
+    @Test
+    public void checkSearchCallCounterForEmptySequence(){
+        similarityFinder.calculateJackardSimilarity(EMPTY_SEQUENCE,SEQUENCE_ONE);
+        assertThat(mockSequenceSearcher.getSearchCallCounter(), is(0));
+    }
+
+
+    @Test
+    public void checkSearchCallCounterOneElementSequence(){
+        similarityFinder.calculateJackardSimilarity(SEQUENCE_ONE,SEQUENCE_ONE);
+        assertThat(mockSequenceSearcher.getSearchCallCounter(), is(1));
+    }
+
+
+    @Test
+    public void checkSearchCallCounterThreeElementSequence(){
+        similarityFinder.calculateJackardSimilarity(SEQUENCE_TWO,SEQUENCE_ONE);
+        assertThat(mockSequenceSearcher.getSearchCallCounter(), is(3));
+    }
+
 }
